78133ddc855a7b1f24e60cbd9eebf1c0f79490e6
Najeeb010 <najeebkm010@gmail.com>
2026-09-01 23:31:37 +0400

    use standard Vercel Express routing

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
 const app = require('../server');
 
-// Vercel rewrites may expose the destination path to the function. Restore the
-// requested route when a rewrite supplies it explicitly.
-module.exports = (req, res) => {
-  const route = new URL(req.url, 'http://localhost').searchParams.get('route');
-  if (route) req.url = route === 'home' ? '/' : `/${route}`;
-  return app(req, res);
-};
+module.exports = app;
